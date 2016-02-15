6afdd120850982183c0aad485ca56c204ba3643f
井上航 <k.prfm19@gmail.com>
2016-02-15 17:10:27 +0900

    Initial Commit

---
+++ TASKEYA_test01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TASKEYA_test01
+//
+//  Created by 井上航 on 2016/02/15.
+//  Copyright © 2016年 Wataru Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
